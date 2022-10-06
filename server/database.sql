d4aedbb4a8e57b8bf05ed12c5c2595abb0c247e5
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-06 13:22:09 +0200

    create fake user

--- server/database.sql
+++ server/database.sql
@@ -7,3 +7,7 @@ CREATE TABLE users(
   user_password VARCHAR(255) NOT NULL,
   PRIMARY KEY(user_id)
 );
+
+-- insert fake user
+
+INSERT INTO users (user_name, user_email, user_password) VALUES ('andrew', 'andrew123@gmail.com', 'andrew123');
