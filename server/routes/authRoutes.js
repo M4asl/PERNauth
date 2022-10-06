e37c202b004503db54c463cbdd2665bd07d1fb13
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-06 14:17:18 +0200

    create login route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,7 +5,7 @@ const pool = require('../db');
 const validInfo = require('../middleware/validInfo');
 const jwtGenerator = require('../utils/jwtGenerator');
 
-//authorizeentication
+// register route -> /auth/register
 
 router.post('/register', validInfo, async (req, res) => {
   const { email, name, password } = req.body;
@@ -30,6 +30,37 @@ router.post('/register', validInfo, async (req, res) => {
 
     const jwtToken = jwtGenerator(newUser.rows[0].user_id);
 
+    return res.json({ jwtToken });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// login route -> /auth/login
+
+router.post('/login', validInfo, async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await pool.query(
+      'SELECT * FROM users WHERE user_email = $1',
+      [email]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).json('Invalid Credential');
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json('Invalid Credential');
+    }
+    const jwtToken = jwtGenerator(user.rows[0].user_id);
     return res.json({ jwtToken });
   } catch (err) {
     console.error(err.message);
