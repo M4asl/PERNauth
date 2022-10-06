8c8c110c04ea8f90f6e391de6a9c5aad87e86af6
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-06 14:13:00 +0200

    create register route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+//routes
+
+app.use('/auth', require('./routes/authRoutes.js'));
+
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
 });
