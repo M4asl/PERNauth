d8bdc179820e2ae4dd9532ce5c477ca0d33550c6
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-07 09:54:23 +0200

    Delete some files & setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+// import { toast } from 'react-toastify';
+
+//components
+
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch('http://localhost:3333/auth/verify', {
+        method: 'POST',
+        headers: { jwt_token: localStorage.token },
+      });
+
+      const parseRes = await res.json();
+
+      parseRes === true
+        ? setIsAuthenticated(true)
+        : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // useEffect(() => {
+  //   checkAuthenticated();
+  // }, []);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="login"
+            element={
+              !isAuthenticated ? (
+                <Login setAuth={setAuth} />
+              ) : (
+                <Navigate to="/dashboard" replace={true} />
+              )
+            }
+          />
+          <Route
+            path="register"
+            element={
+              !isAuthenticated ? (
+                <Register setAuth={setAuth} />
+              ) : (
+                <Navigate to="/dashboard" replace={true} />
+              )
+            }
+          />
+          <Route
+            path="dashboard"
+            element={
+              isAuthenticated ? (
+                <Dashboard setAuth={setAuth} />
+              ) : (
+                <Navigate to="/login" replace={true} />
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
